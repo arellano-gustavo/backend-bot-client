d3316d0f00fbc319bc09c572bb1da822c42f5414
garellano <garellano@10.201.255.120>
2019-09-26 16:53:53 -0500

    Including regular expressions for Mail checking

---
+++ src/test/java/mx/gob/impi/chatbot/persistence/support/MailEngineTest.java
@@ -0,0 +1,35 @@
+package mx.gob.impi.chatbot.persistence.support;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+// Include my Ref, please:
+// https://howtodoinjava.com/regex/java-regex-validate-email-address/
+public class MailEngineTest {
+    private static final Logger logger = Logger.getLogger(MailEngineTest.class);
+    private MailEngine me = new MailEngine();
+    @Test
+    public void pba(){
+        logger.info("Running my test");
+        List<String> emails = new ArrayList<>();
+        emails.add("user@domain.com");
+        emails.add("user@domain.co.in");
+        emails.add("user.name@domain.com");
+        emails.add("user_name@domain.com");
+        emails.add("username@yahoo.corporate.in");
+         
+        //Invalid emails
+        emails.add(".username@yahoo.com");
+        emails.add("username@yahoo.com.");
+        emails.add("username@yahoo..com");
+        emails.add("username@yahoo.c");
+        emails.add("username@yahoo.corporate");
+        
+        assertTrue(me.validateMailStructure(emails.get(0)));
+        
+    }
+}
