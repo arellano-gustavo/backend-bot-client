b042fb35f759f732cf36729afec571f91b00bf9e
garellano <garellano@192.168.100.4>
2019-09-26 22:47:05 -0500

    Clean imports unused

--- src/test/java/mx/gob/impi/chatbot/persistence/support/CustomDigestEncoderTest.java
+++ src/test/java/mx/gob/impi/chatbot/persistence/support/CustomDigestEncoderTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import mx.gob.impi.chatbot.persistence.api.service.CustomDigestEncoderService;
-import mx.gob.impi.chatbot.persistence.api.service.CustomDigestEncoderServiceImpl;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest 
