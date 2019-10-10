38ce49c5bcf674bacd64fa3c771fbba4309bb9ca
Felix <fnunezs@ultrasist.com.mc>
2019-10-10 09:21:08 -0500

    test

--- src/test/java/mx/gob/impi/chatbot/persistence/support/CalculatorTest.java
+++ src/test/java/mx/gob/impi/chatbot/persistence/support/CalculatorTest.java
@@ -14,8 +14,8 @@ public class CalculatorTest {
     public void testSuma() {
         Calculator calc = new Calculator();
         logger.info("Probando el método estático 'suma' de FileUtils");
-        //int result1 = calc.divide(2, 0);
-        //assertTrue("Este es un mensaje de error 1", result1==0);
+        int result1 = calc.divide(2, 0);
+        assertTrue("Este es un mensaje de error 1", result1==0);
         
         int result2 = calc.divide(2, 2);
         assertTrue("Este es un mensaje de error 2", result2==1);
