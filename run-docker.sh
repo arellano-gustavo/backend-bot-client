50f6178d30b107c7e451ff646fcadb8e7b94f854
Felix <fnunezs@ultrasist.com.mc>
2019-10-19 17:37:18 -0500

    ruta despliegue

--- run-docker.sh
+++ run-docker.sh
@@ -3,12 +3,7 @@ echo "Corriendo el aplicativo con un perfil determinado con docker"
 
 docker run -it \
 -p 8080:8080 \
--v /Users/garellano/development/code/impi-chatbot-admin/target:/chat \
-gustavoarellano/jdk18 \
-java -jar \
--Dspring-boot.run.profiles=home \
--Doracle.jdbc.timezoneAsRegion=false \
-/chat/chatbot-persistence-layer-1.0.war
-
+-v /home/chatbot/source-code/impi-chatbot-admin:/chat \
+gustavoarellano/jdk18 
 
 
