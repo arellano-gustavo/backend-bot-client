efd31c54e90ad39782c4f1a1ef826a9dcfdfd326
garellano <garellano@localhost>
2019-10-26 20:52:43 -0500

    Adding a helper for getting properties with profiles & locations

---
+++ src/main/java/mx/gob/impi/chatbot/persistence/support/PropHelper.java
@@ -0,0 +1,93 @@
+package mx.gob.impi.chatbot.persistence.support;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import mx.gob.impi.chatbot.persistence.config.DataConfig;
+
+public class PropHelper {
+    private static final Logger logger = LoggerFactory.getLogger(PropHelper.class);
+    
+    private Properties properties = new Properties();
+    
+    private static PropHelper instance = null;
+
+    public static PropHelper getInstance(String configLocation, String basePropertiesName) throws IOException {
+        if(instance==null) {
+            instance = new PropHelper(configLocation, basePropertiesName);
+        }
+        return instance;
+    }
+    
+    public String getCurrentFirstProfile() {
+        String[] actPro = System.getProperty("spring-boot.run.profiles","").split(",");
+        String activeProfile = "";
+
+        if(actPro[0]!=null && actPro[0].trim().length()>0) {
+            activeProfile = "-"+actPro[0];
+            logger.info("Active profile: [" + actPro[0] + "] <------- Current profile !!!!");
+        } else {
+            logger.error("Couldn't use any profile... Using Profile by default !!!!");
+        }
+        
+        //activeProfile="-impi";
+        //activeProfile="";
+        return activeProfile; // Lleva un "-" al principio
+    }
+    
+    private PropHelper(String configLocation, String basePropertiesName) throws IOException {
+        // calculate paths
+        String activeProfile = this.getCurrentFirstProfile();
+        String fullProfileName = basePropertiesName+activeProfile+".properties";
+        logger.info("fullProfileName: "+fullProfileName);
+        logger.info("configLocation: "+configLocation);
+        
+        
+        // load from classpath or file
+        if(configLocation.trim().length()<1) {
+            logger.info("Loading properties form classpath");
+            InputStream stream =
+                DataConfig
+                .class
+                .getClassLoader()
+                .getResourceAsStream(fullProfileName);
+            properties.load(stream);
+            logger.info("Properties have been loaded from the classpath");
+        } else {
+            logger.info("Loading properties form filesystem");
+            configLocation = configLocation.substring(5);
+            File fr = new File(configLocation+fullProfileName);
+            InputStream fis = new FileInputStream(fr);
+            properties.load(fis);
+            logger.info("Properties ("+basePropertiesName+activeProfile+") have been loaded from the config directory: " + configLocation);
+        }
+    }
+    
+    public Properties getAllProps() {
+        return this.properties;
+    }
+    
+    /**
+     * Regresa la propiedad solicitada
+     * @param name Cadena con el nombre de la propiedad
+     *             que se quiere recuperar
+     * @return Cadena con la propiedad recuperada
+     */
+    public String getProp(String name) {
+        String data = properties.get(name).toString();
+        if("password".equals(name) && data.startsWith("ENC(")) {
+            BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+            textEncryptor.setPassword(name);
+            String pass = data.substring(4, data.length()-1);
+            return textEncryptor.decrypt(pass);
+        }
+        return data;
+    }
+}
