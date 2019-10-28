c7af93fbe5ed2673c024060dbe9b9e22309feb38
garellano <garellano@localhost>
2019-10-27 20:51:54 -0600

    route / to index.html

--- src/main/java/mx/gob/impi/chatbot/persistence/api/controller/StaticController.java
+++ src/main/java/mx/gob/impi/chatbot/persistence/api/controller/StaticController.java
@@ -1,20 +1,22 @@
 package mx.gob.impi.chatbot.persistence.api.controller;
 
-//import java.net.URI;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import java.net.URI;
 
-//@RestController
-//@RequestMapping(value = "/")
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
 public class StaticController {
-//	  @GetMapping(value = "{_:^(?!index\\.html|swagger-ui\\.html|api).$}")
-//	  public ResponseEntity<Void> redirectApi() {
-//	        return ResponseEntity.status(HttpStatus.FOUND)
-//	                .location(URI.create("/index.html"))
-//	                .build();
-//	  }
+	//@GetMapping(value = "{_:^(?!index\\.html|swagger-ui\\.html|api).$}")
+	@GetMapping(value = "/")
+	public ResponseEntity<Void> redirectApi() {
+	      return ResponseEntity
+	    		  .status(HttpStatus.FOUND)
+	              .location(URI.create("/index.html"))
+	              .build();
+	}
 }
